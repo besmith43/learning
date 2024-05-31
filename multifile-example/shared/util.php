8d8edb357c3f65d9f85f731b90494b33a681da80
Blake Smith <besmith43@gmail.com>
2024-05-31 09:36:02 -0500

    basic multifile example

---
+++ multifile-example/shared/util.php
@@ -0,0 +1,7 @@
+<?php
+
+function MyLogger($strMessage) {
+    echo $strMessage;
+}
+
+?>
---
+++ multifile-example/start.php
@@ -0,0 +1,8 @@
+<?php
+require 'shared/util.php';
+
+$strThingy = "This is a test of the emergency broadcast system";
+
+MyLogger($strThingy);
+
+?>
