a1dde7ba07cf9e4abc9597496d8252d73965b41e
Blake Smith <besmith43@gmail.com>
2024-07-19 12:57:31 -0500

    added some new features as examples

--- scripting/script_lib/main.go
+++ scripting/script_lib/main.go
@@ -19,4 +19,17 @@ func main() {
 	}
 
 	fmt.Println("test.txt Contents:", contents)
+
+	script.Get("https://wttr.in/London?format=3").Stdout()
+
+	// you need the trailing \n because echo isn't doing a new line on it's own
+	script.Echo("data\n").Tee().AppendFile("data.txt")
+
+	numLines, err := script.File("data.txt").CountLines()
+	if err != nil {
+		fmt.Println("Error with data.txt:", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Data.txt has %d lines\n", numLines)
 }
