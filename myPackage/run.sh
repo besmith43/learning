874fea4166ac21d2e79a3021b4c19085cc402e36
Blake Smith <besmith43@gmail.com>
2024-09-24 20:16:45 -0500

    saving progress

---
+++ myPackage/run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+if [ ! -d bin ]; then
+	mkdir bin
+elif [ -d bin ]; then
+	rm -r bin
+	mkdir bin
+fi
+
+# javac -d bin src/Math.java
+javac -d bin src/Math.java src/main.java
+# javac -d bin src/main.java
+
+java -cp ./bin myPackage.Main
+
---
+++ myPackage/src/math.java
@@ -0,0 +1,7 @@
+package myPackage;
+
+public class Math {
+	public static int Add(int x, int y) {
+		return x + y;
+	}
+}
