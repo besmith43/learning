b85a78c479862a191b68dcf550703099135afbe2
Blake Smith <besmith43@gmail.com>
2024-10-08 10:04:12 -0500

    saving progress

---
+++ catching_pipes/main.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo outputing all params
+echo $*
+
+echo
+echo
+
+echo outputing piped data
+read pipedText
+echo $pipedText
+
+echo
+echo
+
+
---
+++ catching_pipes/run.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+
+echo "this is a test of the emergency broadcast systems" | ./main.sh debug
+
