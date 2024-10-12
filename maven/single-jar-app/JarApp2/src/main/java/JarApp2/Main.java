c7e70efee7d1298b69f2fb7d674704e4f9ed6cbf
Blake Smith <besmith43@gmail.com>
2024-10-12 15:19:50 -0500

    saving progress

---
+++ maven/single-jar-app/JarApp2/src/main/java/JarApp2/Main.java
@@ -0,0 +1,12 @@
+package JarApp2;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args ) {
+		System.out.println("Hello World!");
+    }
+}
---
+++ maven/single-jar-app/create.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+
+# mvn archetype:generate -DarchetypeGroupId=remote -DarchetypeArtifactId=com.matthewjosephtaylor.archetypes:application-archetype
+
+echo use id 15!!!!
+
+mvn archetype:generate -DarchetypeGroupId=remote -DarchetypeArtifactId=com.github.igor-petruk.archetypes:maven-archetype-executable
+
+
+
