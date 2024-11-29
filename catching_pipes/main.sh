ba4b13a775f2a3a6356e96d2fc750945af74076a
Blake Smith <besmith43@gmail.com>
2024-11-28 21:09:04 -0600

    saving progress

--- catching_pipes/main.sh
+++ catching_pipes/main.sh
@@ -17,4 +17,14 @@ fi
 echo
 echo
 
-
+# references
+#
+# the -t 0 comes from here
+# https://stackoverflow.com/questions/2456750/how-to-check-if-stdin-is-from-the-terminal-or-a-pipe-in-a-shell-script
+# the short explanation is that test -t 0 is checking for piped stdin
+# while test -t 1 would be checking for piped stdout
+# and test -t 2 would be checking for piped stderr
+#
+#
+#
+#
