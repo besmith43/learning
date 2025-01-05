933d4af97b440c7dc64ac6f45a88924504cf2eee
Blake Smith <besmith43@gmail.com>
2025-01-05 07:21:46 -0600

    saving progress

--- check-for-unwrap.sh
+++ check-for-unwrap.sh
@@ -4,6 +4,8 @@
 echo "listing every use of unwrap in your codebase"
 grep -ir unwrap --include \*.rs
 
-echo "the total count of instances found"
+echo
+echo
+printf "the total count of instances found:"
 grep -ir unwrap --include \*.rs | wc -l
 
