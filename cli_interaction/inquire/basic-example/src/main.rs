a1d8aaedebbe16f818d01d4daff348ebf679e1ff
Blake Smith <besmith43@gmail.com>
2025-01-06 16:01:32 -0600

    saving progress

---
+++ cli_interaction/inquire/basic-example/src/main.rs
@@ -0,0 +1,12 @@
+use inquire::{Select, InquireError};
+
+fn main() {
+    let options: Vec<&str> = vec!["Banana", "Apple", "Strawberry", "Grapes", "Lemon", "Tangerine", "Watermelon", "Orange", "Pear", "Avocado", "Pineapple"];
+
+    let ans: Result<&str, InquireError> = Select::new("What's your favorite fruit?", options).prompt();
+
+    match ans {
+        Ok(choice) => println!("{}! That's mine too!", choice),
+        Err(_) => println!("There was an error, please try again"),
+    }
+}
