d40d0e1f2fa1b67e362cd53223c1bbc3ff23493a
Blake Smith <besmith43@gmail.com>
2025-01-14 13:17:59 -0600

    saving progress

---
+++ formatting/basic-example/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("Hello, world!");
+
+    let num1 = 1;
+    let num2 = 2;
+    let num3 = 3;
+    let num4 = 4;
+    let num5 = 5;
+
+
+    println!("{} {} {} {} {}",
+        num1,
+        num2,
+        num3,
+        num4,
+        num5,
+    );
+}
