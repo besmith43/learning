a1bd73439a43a01df1f7f73cf743170280208e75
Blake Smith <besmith43@gmail.com>
2025-04-01 14:08:34 -0400

    saving progress

--- plugins/my-basic-example/main.go
+++ plugins/my-basic-example/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"errors"
 	"fmt"
+	"io/fs"
 	"log"
+	"os"
+	"path"
 	"plugin"
 )
 
@@ -14,16 +17,47 @@ type WebApp interface {
 func main() {
 	fmt.Println("Hello World!")
 
-	response, err := LoadPlugin()
-	if err != nil {
-		log.Fatal(err)
-	}
+	run_for_loop()
 
-	fmt.Println(response())
+	run_glob()
 }
 
-func LoadPlugin() (func(), error) {
-	plugin, err := plugin.Open("bin/plugin1.so")
+func run_glob() {
+	binPath := "bin"
+
+	plugins := listFiles(binPath)
+
+	fmt.Println("running glob")
+
+	for _, v := range plugins {
+
+		response, err := LoadPlugin(v)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(response())
+
+	}
+}
+
+func run_for_loop() {
+
+	fmt.Println("running count based for loop")
+
+	for count := range 3 {
+
+		response, err := LoadPlugin(fmt.Sprintf("bin/plugin%d.so", count+1))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(response())
+	}
+}
+
+func LoadPlugin(path string) (func() string, error) {
+	plugin, err := plugin.Open(path)
 	if err != nil {
 		return nil, err
 	}
@@ -33,10 +67,26 @@ func LoadPlugin() (func(), error) {
 		return nil, err
 	}
 
-	webappfunc, ok := symWebApp.(func())
+	webappfunc, ok := symWebApp.(func() string)
 	if !ok {
 		return nil, errors.New("unexpected type from module symbol")
 	}
 
 	return webappfunc, nil
+}
+
+func listFiles(dir string) []string {
+	root := os.DirFS(dir)
+
+	mdFiles, err := fs.Glob(root, "*.so")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var files []string
+	for _, v := range mdFiles {
+		files = append(files, path.Join(dir, v))
+	}
+	return files
 }
--- plugins/my-basic-example/run.sh
+++ plugins/my-basic-example/run.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+if [ ! -d ./bin ]; then
+    mkdir bin
+fi
+
 go build --buildmode=plugin -o ./bin/plugin1.so ./plugin1/plugin1.go
+go build --buildmode=plugin -o ./bin/plugin2.so ./plugin2/plugin2.go
+go build --buildmode=plugin -o ./bin/plugin3.so ./plugin3/plugin3.go
 
 
 go run main.go
