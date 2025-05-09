514f3b49eebde718c25094f4c50649892b93aa4f
Blake Smith <besmith43@gmail.com>
2025-05-09 11:11:38 -0400

    I really dislike the build tooling

---
+++ picocli_lib/basic_example/hello/native-compile.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+
+# have to source sdkman even
+# if it's been sourced in the parent shell
+if [ -f $HOME/.sdkman/bin/sdkman-init.sh ]; then
+    source $HOME/.sdkman/bin/sdkman-init.sh
+fi
+
+current_jdk="$(sdk list java | grep ">>>" | awk '{ print $(NF) }')"
+graal_installed="$(sdk list java | grep graalce | grep installed | head -n 1)"
+reset_jdk=false
+
+if [[ "$current_jdk" != *"graalce"* ]]; then
+    echo "current jdk is not graal"
+    reset_jdk=true
+fi
+
+if [ -z "$graal_installed" ]; then
+    echo "graal is not installed"
+    graal_version="$(sdk list java | grep graalce | head -n 1 | awk '{ print $(NF) }')"
+    sdk install java $graal_version
+fi
+
+if [ -z "$(which native-image)" ]; then
+    echo "native-image binary not found in path"
+    exit 1
+fi
+
+
+mvn clean package
+
+if [ $? -ne 0 ]; then
+    exit $?
+fi
+
+# mvn -Pnative package
+
+native-image -jar target/hello.jar
+
+
+if $reset_jdk ; then
+    echo sdk use java $current_jdk
+fi
+
