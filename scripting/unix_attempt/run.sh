8e808fe24f920e9d2902042ad624437885856453
Blake Smith <besmith43@gmail.com>
2025-05-16 19:09:02 -0400

    saving progress

--- scripting/unix_attempt/run.sh
+++ scripting/unix_attempt/run.sh
@@ -3,6 +3,16 @@
 
 export CLASS_PATH="$HOME/.icloud/java_classpath"
 
+echo "running DoWork with no piped input"
+
+./DoWork
+
+echo
+echo
+
+echo "running DoWork with all the fixin's"
+
 cat input.txt | ./DoWork testing | sed -e 's/Path/PATH/'
 
+echo
 
