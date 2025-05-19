3cb040b1eec377a2e6b1ce714302387e4efb4ebe
Blake Smith <besmith43@gmail.com>
2025-05-19 06:42:35 -0400

    saving progress

--- logging/basic_java_util_logging_example/src/java/Main.java
+++ logging/basic_java_util_logging_example/src/java/Main.java
@@ -4,6 +4,12 @@ import java.util.logging.Level;
 
 // code came from here: https://www.digitalocean.com/community/tutorials/logger-in-java-logging-example
 
+// see StackOverflow for how to add a file handler and formatter to this
+// https://stackoverflow.com/questions/15758685/how-to-write-logs-in-text-file-when-using-java-util-logging-logger
+
+// logging levels in java.util.logging
+// SEVERE, WARNING, INFO, CONFIG, FINE, FINER, FINEST
+
 public class Main {
     public static void main(String[] args) {
         Logger logger = Logger.getLogger(Main.class.getName());
@@ -11,7 +17,7 @@ public class Main {
         // LogManager.getLogManager().readConfiguration(new FileInputStream("mylogging.properties"));
 
         logger.log(Level.INFO, "This is an info message.");
-        logger.log(Level.FINE, "This is a debug message.");
+        logger.log(Level.FINE, "This is a fine message.");
         logger.log(Level.WARNING, "this is a warning!");
         logger.log(Level.SEVERE, "This is an error message.");
     }
