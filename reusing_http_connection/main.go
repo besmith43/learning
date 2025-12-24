cc9816f568d72f00189d23e836953ca7b1b1792b
Blake Smith <besmith43@gmail.com>
2025-12-23 18:59:53 -0600

    adding http connection reuse example

---
+++ reusing_http_connection/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httptrace"
+)
+
+// code example comes from: https://golang.cafe/blog/how-to-reuse-http-connections-in-go
+
+func main() {
+	// client trace to log whether the request's underlying tcp connection was re-used
+	clientTrace := &httptrace.ClientTrace{
+		GotConn: func(info httptrace.GotConnInfo) { log.Printf("conn was reused: %t", info.Reused) },
+	}
+	traceCtx := httptrace.WithClientTrace(context.Background(), clientTrace)
+
+	// 1st request
+	req, err := http.NewRequestWithContext(traceCtx, http.MethodGet, "http://example.com", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := io.Copy(ioutil.Discard, res.Body); err != nil {
+		log.Fatal(err)
+	}
+	res.Body.Close()
+	// 2nd request
+	req, err = http.NewRequestWithContext(traceCtx, http.MethodGet, "http://example.com", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
