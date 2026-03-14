9e4611b2d5e3b9d2fadfe3636c2280de020674fc
Blake Smith <besmith43@gmail.com>
2026-03-14 10:00:32 -0400

    updating starting point

---
+++ firstJavaGuiApp/native-image.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+javac --release 8 ./GUI.java
+
+# Create the JAR with resources and manifest
+jar --create --file GUI.jar --manifest manifest.txt GUI.class img/
+
+
+native-image --jar ./GUI.jar
+
