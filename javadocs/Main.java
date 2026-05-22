0d1c0cbb7e6add6fd45aa412340f1687c72bca05
Blake Smith <besmith43@gmail.com>
2026-05-22 07:48:30 -0400

    adding javadoc example

---
+++ javadocs/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("converted number: %d\n", processData("1"));
+    }
+
+    /**
+        * Summary sentence: This is what the method does.
+        *
+        * @param input Description of the input parameter.
+        * @return Description of what the method returns.
+    */
+    public static int processData(String input) {
+        return Integer.parseInt(input);
+    }
+}
---
+++ javadocs/build_javadocs.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+
+javadoc -d output Main.java
+
